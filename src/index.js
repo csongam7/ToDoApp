04e24a6cdff5c89be1ec6ae254e226bdbc700f3a
csongam7 <csongam7@gmail.com>
2024-08-05 11:05:57 +0200

    webpack hot module reload enabled

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const sideContainer = document.createElement('div');
+const toDoContainer = document.createElement('div');
+document.body.appendChild(sideContainer)
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: {
+     app: './src/index.js',
+  },
   mode: 'development',
-  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+   hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Hot Module Replacement',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Directory to serve static files from
+    },
+    compress: true, // Enable gzip compression
+    port: 9000, // Port to run the server on
+    open: true, // Automatically open the browser
+    hot: true, // Enable Hot Module Replacement
   },
 };
-
